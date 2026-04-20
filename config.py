959dbf4797b0831bdaad8251e5d42744464b33c3
lakshmipotugari1423-max <lakshmipotugari1423@gmail.com>
2026-04-20 17:08:59 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+FILE_TYPES = {
+    "Images": [".jpg", ".png", ".jpeg", ".gif"],
+    "Documents": [".pdf", ".docx", ".txt"],
+    "Videos": [".mp4", ".mkv"],
+    "Music": [".mp3", ".wav"],
+    "Archives": [".zip", ".rar"]
+}
